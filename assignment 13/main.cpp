219ff40914886ae520bc5604c192d812550e228d
tdragon00 <47230462+tdragon00@users.noreply.github.com>
2019-12-02 13:02:35 -0800

    stuff2

---
+++ assignment 13/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 2, 2019
+ *      Author: Slickback
+ */
+#include <node.h>
+#include <linkedlist.h>
+using std;
+int main()
+{
+
+	std::cout<<"Part one";
+	int arr = {1,2,3,4,5};
+	int size =5;
+
+	List<int>intLList;
+	for (int i=0; i<= size-1; i++)
+	{
+		intLList.insertNewNode(arr<i>);
+	}
+
+
+
+
+
+}
+
+
